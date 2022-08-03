6a3d050502b5c823044faab145c30bf407fe4026
Furkan Taşdemir <furkantsdmr97@gmail.com>
2022-08-03 13:14:13 +0300

    backend-completed

---
+++ server/routes/auth.js
@@ -0,0 +1,17 @@
+import  express  from "express";
+import {  } from "../controllers/auth.js";
+
+const router = express.Router()
+
+// CREATE A USER 
+router.post("/signup",)
+
+
+// SIGN IN 
+router.post("/signup",)
+
+// GOOGLE AUTH
+router.post("/signup",)
+
+
+export default router;
